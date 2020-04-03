e4ec0b34221979ddc6ce1e591e8d0391e4c82936
Soumya Kanti Das <das.soumyakanti6@gmail.com>
2020-04-03 17:13:31 +0530

    signup fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 	skip_before_action :verify_authenticity_token
 	
 	def configure_permitted_parameters  
-		devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:username,:email])
 		devise_parameter_sanitizer.permit(:sign_in, keys: [:login])
 	end
 end
